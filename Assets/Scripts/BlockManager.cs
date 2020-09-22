fd0f1f5015b49bf2e2bc24f605160b5ed87241eb
KKWANH <kwanho0096@gmail.com>
2020-09-22 15:27:33 +0900

    First Commit
    메뉴만 넣었습니다. 걍 기본 사항만 넣었고 차차 추가할거임

---
+++ Assets/Scripts/BlockManager.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public		class		BlockManager : MonoBehaviour
+{
+	public	int			Block;
+    public	void		setBlock(int block) {
+
+	}
+}
---
+++ Assets/Scripts/SceneManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public	class	SceneManager : MonoBehaviour
+{
+    public	void	Scene_0_Title() {
+		UnityEngine.SceneManagement.SceneManager.LoadScene("0_Title");
+	}
+
+    public	void	Scene_1_InGame() {
+		UnityEngine.SceneManagement.SceneManager.LoadScene("1_InGame");
+	}
+
+	public	void	Scene_2_AboutGame() {
+		UnityEngine.SceneManagement.SceneManager.LoadScene("2_AboutGame");
+	}
+
+	public	void	Popup_0_OptionDefault() {
+		
+	}
+}
